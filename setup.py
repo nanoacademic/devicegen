120362b9546722f926c9650612aa0662bc6adf7f
Félix Beaudoin <fbeaudoin@nanoacademic.com>
2022-02-24 20:57:49 -0500

    Added license information in setup.py.

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ setup(
     name="devicegen",
     packages=find_packages(),
     version="0.0.0",
+    license='GPLv3+',
     author="Pericles Philippopoulos, Felix Beaudoin",
     author_email="pericles@nanoacademic.com",
     description="Device generator for gated quantum devices",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Topic :: Scientific/Engineering",
     ],
 )
